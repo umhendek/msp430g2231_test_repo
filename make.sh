b52f986bfaa006d520ae063c07091da64fe9a5b4
kevin henderson <umhendek@gmail.com>
2013-02-01 00:21:20 -0600

    updating make

--- make.sh
+++ make.sh
@@ -1,3 +1,9 @@
+#	
+#	Author: Kevin Henderson
+#	Email: umhendek@gmail.com
+#	Date: 12-28-12
+#
+
 msp430-gcc -Os -mmcu=msp430g2231 -o test.elf test.c
 msp430-objdump -DS test.elf > test.lst
 msp430-objcopy -O ihex test.elf test.hex
